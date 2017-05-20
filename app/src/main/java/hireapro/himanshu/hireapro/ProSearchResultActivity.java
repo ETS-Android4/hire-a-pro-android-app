6963f11161c532a5c91c8e3211da1b9dcbfc3e2a
Deadpool <him.khati@gmail.com>
2017-05-20 12:13:39 +0530

    Pro Search with Collapsable toolbar added

--- app/src/main/java/hireapro/himanshu/hireapro/ProSearchResultActivity.java
+++ app/src/main/java/hireapro/himanshu/hireapro/ProSearchResultActivity.java
@@ -2,6 +2,7 @@ package hireapro.himanshu.hireapro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ProSearchResultActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class ProSearchResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pro_search_result);
+        Toolbar toolbar =  (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if(getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
